d247c72d2a50ecb9e0465466018a089ef49f94cb
andreychernykh@me.com <andreychernykh@me.com>
2020-10-09 16:12:26 +0300

    add bot response

--- src/store.js
+++ src/store.js
@@ -1,6 +1,27 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {createBrowserHistory} from 'history';
+import storage from 'redux-persist/lib/storage';
+import {createRootReducer} from 'reducers';
+import {routerMiddleware} from 'connected-react-router';
+import {botMiddleware} from './middlewares/botMiddleware';
+import {persistStore, persistReducer} from 'redux-persist';
 
-import {rootReducer} from 'reducers';
+export const history = createBrowserHistory();
 
-export const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+    key: 'app',
+    storage,
+};
+
+export const initStore = () => {
+    const initialStore = {};
+    const store = createStore(
+        persistReducer(persistConfig, createRootReducer(history)),
+        initialStore,
+        composeWithDevTools(
+            applyMiddleware(botMiddleware, routerMiddleware(history)),
+        ));
+    const persistor = persistStore(store);
+    return {store, persistor};
+};
